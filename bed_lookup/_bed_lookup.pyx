09df3e4a1e6b36f3609b26b4839ca7b323a40d51
Mike Dacre <mike.dacre@gmail.com>
2015-12-14 12:05:10 -0800

    Attempted to reduce overhead

--- bed_lookup/_bed_lookup.pyx
+++ bed_lookup/_bed_lookup.pyx
@@ -7,24 +7,27 @@ class BedFile():
     """ A Lookup Object """
     def __init__(self, bedfile):
         self.dict = {}
+        cdef int start, end
         with open(bedfile) as infile:
             for line in infile:
                 f = line.rstrip().split('\t')
                 if len(f) < 4:
                     continue
-                chr   = str(f[0])
+                chr   = f[0]
                 start = int(f[1])
                 end   = int(f[2])
-                gene  = str(f[3])
+                gene  = f[3]
                 if chr not in self.dict:
                     self.dict[chr] = {}
                 self.dict[chr][gene] = (start, end)
 
     def lookup(self, chromosome, location):
         """ Lookup your gene. Returns the gene name """
+        cdef int l
+        l = int(location)
         answer = ''
         for k, v in self.dict[chromosome].items():
-            if v[0] < location < v[1]:
+            if v[0] < l < v[1]:
                 answer = k
                 break
         if answer:
